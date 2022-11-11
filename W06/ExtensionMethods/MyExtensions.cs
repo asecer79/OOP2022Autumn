f148a85693698f5fff5c3fe1952c944f39a27bf0
Aydin Secer <Aydin Secer@DESKTOP-7LBD65T>
2022-11-11 10:34:27 +0300

    u

---
+++ W06/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W06.ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static int ToInt(this string value)
+        {
+            return Convert.ToInt32(value);
+        }
+
+        public static int ToSquare(this int value)
+        {
+            return value * value;
+        }
+
+        public static double DotProd(this Point2D point1, Point2D point2)
+        {
+            return point1.X * point2.X + point1.Y * point2.Y;
+        }
+
+        public static double Distance(this Point2D point1, Point2D point2)
+        {
+            return Math.Sqrt(Math.Pow(point1.X - point2.X,2) + Math.Pow(point1.Y - point2.Y,2));
+        }
+
+        //1 char shift to the right
+        public static string Encrypt(this string value,int shiftLevel)
+        {
+            var encrypted = "";
+
+            foreach (var c in value)
+            {
+                encrypted +=((char) (c + shiftLevel)).ToString();
+            }
+
+            return encrypted;
+        }
+
+        public static string Decrypt(this string value, int shiftLevel)
+        {
+            var decrypted = "";
+
+            foreach (var c in value)
+            {
+                decrypted += ((char)(c - shiftLevel)).ToString();
+            }
+
+            return decrypted;
+        }
+
+        public static string TersCevir(this string value)
+        {
+            var reversed = "";
+            var len = value.Length;
+
+            for (int i = len-1; i >= 0; i--)
+            {
+                reversed += ((char)value[i]).ToString();
+            }
+          
+
+            return reversed;
+        }
+    }
+}
---
+++ W06/Program.cs
@@ -0,0 +1,200 @@
+﻿
+using W06.ExtensionMethods;
+
+namespace W06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Point p1 = new Point();
+            //p1.X = 1;
+            //p1.Y = 2;
+
+
+            //Point2D p2 = new Point2D();
+            //p2.X = 1;
+            //p2.Y = 2;
+
+            //Console.WriteLine($"for struct: p1.X, p1.Y = {p1.X}, {p1.Y}, for class: p2.X, p2.Y = {p2.X}, {p2.Y}");
+
+            //MultiplyByConstant(p1, 2);
+            //MultiplyByConstant(p2, 2);
+
+            //Console.WriteLine($"for struct: p1.X, p1.Y = {p1.X}, {p1.Y}, for class: p2.X, p2.Y = {p2.X}, {p2.Y}");
+
+            ////.. here Point p1 has not changed
+            //.. here Point2D p2 has changed
+
+
+            //int num1 =  Console.ReadLine().ToInt();
+            //int num2 = Console.ReadLine().ToInt();
+
+           // Console.WriteLine(num1+num2);
+         
+            //Console.WriteLine("1".ToInt()+"2".ToInt());
+
+            //Console.WriteLine(25.ToSquare());
+
+
+            //Point2D p1 = new Point2D(){X = 2,Y = 3};
+            //Point2D p2 = new Point2D() { X = -2, Y = -3 };
+
+            //var result = p1.DotProd(p2);
+            //Console.WriteLine(result);
+
+            //Console.WriteLine(p1.Distance(p1));
+            var clearText = "Aydın";
+
+            //var enc = clearText.Encrypt(15);
+            //Console.WriteLine(enc);
+
+            //var dec = enc.Decrypt(15);
+            //Console.WriteLine(dec);
+
+            Console.WriteLine(clearText.TersCevir());
+        }
+
+        static void MultiplyByConstant(Point2D point,int num)
+        {
+            point.X = point.X * num;
+            point.Y = point.Y* num;
+
+            Console.WriteLine($"for class: p2.X, p2.Y = {point.X}, {point.Y}");
+        }
+        static void MultiplyByConstant(Point point, int num)
+        {
+            point.X = point.X * num;
+            point.Y = point.Y * num;
+
+            Console.WriteLine($"for struct: p2.X, p2.Y = {point.X}, {point.Y}");
+        }
+    }
+
+
+    //reference type
+    public class Point2D
+    {
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Module()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+        }
+    }
+
+
+    #region Structs
+    //P(x,y)
+    //value type
+    public struct Point
+    {
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Module()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+        }
+    }
+
+    #endregion
+
+    #region Class Types
+
+    //regular normal class/
+    //can be instantiated,by new keyword
+    //can be inherited,
+    //can implement  interfaces
+    //can inherit,
+    //may have static and instance members
+    public class Human 
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
+    //abstract class
+    //can not be initialized/ by new keyword
+    //can be inherited
+    //can inherit
+    //can implement  interfaces
+    //may have static and instance members
+    // can be used as Interfaces
+    public abstract class MathOperation
+    {
+        public double PI { get; set; }
+
+         static MathOperation()
+        {
+
+        }
+        //concrete
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        //abstract method
+        public abstract  int Divide(int a, int b);
+    }
+
+
+
+    //static class
+    //can not be initialized/ by new keyword
+    //can not be inherited
+    //all members must be static
+    //can inherit
+    //can not implement  interfaces
+
+    public static class Algebra
+    {
+        public static double PI { get; set; }
+        public static double Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+
+
+
+    public interface ITestInterface
+    {
+        void TestMethod1(int a, int b);
+    }
+
+
+
+    //sealed class
+    //can be initialized/ by new keyword
+    //can not be inherited
+    //can inherit
+    //can  implement  interfaces
+
+   public sealed class LivingCell
+    {
+        public static  int Number { get; set; }
+        public string Goup { get; set; }
+
+        public void Breath()
+        {
+            Console.WriteLine("Breathing");
+        }
+    }  
+   
+   
+   #endregion
+}
+
+
+
+
+
